ef63c886c5e5cae01972e4d6a392b4964c0a3024
itsanti <kurov.aleksandr+github@gmail.com>
2018-10-22 17:39:35 +0300

    HW3: process

--- app/src/main/java/ru/vkwebsites/android/simpleweatherapp/MainActivity.java
+++ app/src/main/java/ru/vkwebsites/android/simpleweatherapp/MainActivity.java
@@ -1,13 +1,57 @@
 package ru.vkwebsites.android.simpleweatherapp;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText city;
+    private Button btnShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        setOnShowClickListener();
+    }
+
+    private void initViews() {
+        city = findViewById(R.id.city);
+        btnShow = findViewById(R.id.btnShow);
+    }
+
+    private void setOnShowClickListener() {
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), city.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("city", city.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        city.setText(savedInstanceState.getString("city", ""));
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // можно посмотреть как изменилась конфигурация
+        // newConfig.orientation == Configuration.ORIENTATION_PORTRAIT
     }
 }
